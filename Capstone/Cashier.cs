677e3d73931b27881815ef3d51f1fdf920cea134
Mac Reske <richard.m.reske@Gmail.com>
2023-10-05 14:10:33 -0400

    Cashier Class -End Transaction

--- Capstone/Cashier.cs
+++ Capstone/Cashier.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Capstone
 {
@@ -33,12 +29,34 @@ namespace Capstone
             else
             {
                 Console.WriteLine("This item costs more than the current balance. Please insert more bills.");
+                Console.ReadKey();
             }
         }
         public void EndTransaction()
         {
-            Console.WriteLine($"Transaction Complete, your change is {Balance}");
-            
+            decimal total = Balance;
+            int quarters =0;
+            int nickels =0;
+            int dimes =0;
+
+            while (Balance >= .25M)
+            {
+                Balance -= .25M;
+                quarters++;
+            }
+            while (Balance >= .10M)
+            {
+                Balance -= .10M;
+                dimes++;
+            }
+            while (Balance >= .05M)
+            {
+                Balance -= .05M;
+                nickels++;
+            }
+
+            Console.WriteLine($"Transaction Complete, your change is: {total} /n Quarters: {quarters} /n Dimes: {dimes} /n Nickels: {nickels}");
+            Console.ReadKey();
         }
     }
 }
--- Capstone/Inventory.cs
+++ Capstone/Inventory.cs
@@ -1,10 +1,6 @@
 ﻿using Capstone.Plush;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Capstone
 {
